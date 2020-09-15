e30e8dc3cca256d21017fe696c9e3302a7d0de91
unknown <epgdatacapbon@users.noreply.github.com>
2020-09-15 23:59:02 +0900

    Update sample scripts

--- samples/push.bat
+++ samples/push.bat
@@ -1,9 +1,18 @@
+@echo off
+
 set APPLICATION=App-Package-Name
 set TITLE=Push Client
 
 set MESSAGE=[%date:~5% %time:~,5%] %~1 "<%~2>"
 set APIKEY=AAAALPnr2wM:APA91bEoMDt4GgFZWEx5-RwzYOzNcZkk4pr3RdDQgME4Oip8fA39XcC3hEzvpR3z2Caim1vTqGGr7JNG_ITZIe1jgSHhdi2t2kHllEW5Jy2Z5mG7xv5GHfoqeHO-1AVp9blJcE0AG3Cb
 
+setlocal EnableDelayedExpansion
+
 for /f "usebackq" %%I in ("%~dp0push.dat") do (
-	curl -k --header "Authorization: key=%APIKEY%" --header Content-Type:"application/json" https://fcm.googleapis.com/fcm/send -d "{\"to\":\"%%I\",\"collapse_key\":\"pushclient\",\"priority\":\"high\",\"content_available\":true,\"data\":{\"app\":\"%APPLICATION%\",\"title\":\"%TITLE%\",\"msg\":\"%MESSAGE%\"}}"
+	set IDS=!IDS!,\"%%I\"
 )
+set IDS=%IDS:~1%
+
+curl -k --header "Authorization: key=%APIKEY%" --header Content-Type:"application/json" https://fcm.googleapis.com/fcm/send -d "{\"registration_ids\":[%IDS%],\"collapse_key\":\"pushclient\",\"priority\":\"high\",\"direct_book_ok\":true,\"data\":{\"app\":\"%APPLICATION%\",\"title\":\"%TITLE%\",\"msg\":\"%MESSAGE%\"}}"
+
+endlocal
--- samples/push.sh
+++ samples/push.sh
@@ -7,13 +7,11 @@ DATE=`date '+%m/%d %R'`
 MESSAGE="[$DATE] $1 <$2>"
 APIKEY=AAAALPnr2wM:APA91bEoMDt4GgFZWEx5-RwzYOzNcZkk4pr3RdDQgME4Oip8fA39XcC3hEzvpR3z2Caim1vTqGGr7JNG_ITZIe1jgSHhdi2t2kHllEW5Jy2Z5mG7xv5GHfoqeHO-1AVp9blJcE0AG3Cb
 
-cnt=3
-while read token; do
-	for i in `seq $cnt`; do
-		RESULT=`curl -k --header "Authorization: key=$APIKEY" --header Content-Type:"application/json" https://fcm.googleapis.com/fcm/send -d "{\"to\":\"$token\",\"collapse_key\":\"pushclient\",\"priority\":\"high\",\"content_available\":true,\"data\":{\"app\":\"$APPLICATION\",\"title\":\"$TITLE\",\"msg\":\"$MESSAGE\"}}"`
-		if `echo $RESULT | grep -sq "\"success\":1"` || [ $i -eq $cnt ]; then
-			break
-		fi
-		sleep 1
-	done
+while read TOKEN; do
+	if [ -n "$TOKEN" ]; then
+		IDS="$IDS",\"$TOKEN\"
+	fi
 done < `dirname $0`/push.dat
+IDS=`echo "$IDS" | sed 's/^.//'`
+
+curl -k --header "Authorization: key=$APIKEY" --header Content-Type:"application/json" https://fcm.googleapis.com/fcm/send -d "{\"registration_ids\":[$IDS],\"collapse_key\":\"pushclient\",\"priority\":\"high\",\"direct_book_ok\":true,\"data\":{\"app\":\"$APPLICATION\",\"title\":\"$TITLE\",\"msg\":\"$MESSAGE\"}}"
